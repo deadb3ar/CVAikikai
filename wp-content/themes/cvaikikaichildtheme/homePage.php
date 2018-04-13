e8febc7f252f9c8f44aba45c11d46c7b65197771
Zoe Soroka <zoesoroka@hotmail.com>
2018-04-13 07:07:12 -0700

    finished home/about page and general set up

--- wp-content/themes/cvaikikaichildtheme/homePage.php
+++ wp-content/themes/cvaikikaichildtheme/homePage.php
@@ -2,12 +2,24 @@
 
 get_header(); 
 ?>
+
+<img id="heroImg" src="wp-content/themes/cvaikikaichildtheme/assets/comox-valley-aikikai-aikido-gi-frontpage.jpg">
+<img id="heroLogo" src="wp-content/themes/cvaikikaichildtheme/assets/comox-valley-aikikai-aikido-logo.png">
+
 <main id="main" class="site-main content-area full-width single-post homeCenter" role="main">
-  
+
+
 <div class="homeTest">
-    <p><b>Section one:</b><br> Aikido is a Japanese martial art where training is used as a way for personal development, self-discipline and spiritual growth. The ideal in Aikido is not to think of defeating attackers, but rather to be in harmony with them while neutralizing their attacks. The nature of Aikido philosophy is to promote harmony and non-conflict:  there is no "sparring" during practice. Rather, training partners help each other learn the proper execution of various forms.
+    <p><b>section one:</b><br><br> Aikido is a Japanese martial art where training is used as a way for personal development, self-discipline and spiritual growth. The ideal in Aikido is not to think of defeating attackers, but rather to be in harmony with them while neutralizing their attacks. The nature of Aikido philosophy is to promote harmony and non-conflict:  there is no "sparring" during practice. Rather, training partners help each other learn the proper execution of various forms.
     </p>
     <img src="wp-content/themes/cvaikikaichildtheme/assets/aikido-hand-stretch-intro-to-aikido-comox-valley.png" alt="akido hand stretch">
+</div>
+
+<div id="dropIn">
+    <div>
+        <p>DROP IN ANYTIME!</p>
+        <button id="dropInButton">Click here to <b>get started<b></button>
+    </div>
 </div>
 
 </main>
