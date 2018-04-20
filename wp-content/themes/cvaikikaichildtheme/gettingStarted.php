1e8d1170abb21d3c3bfe25b0fc5045aca26ab52a
Zoe Soroka <zoesoroka@hotmail.com>
2018-04-20 00:46:07 -0700

    middleBox class won't work

--- wp-content/themes/cvaikikaichildtheme/gettingStarted.php
+++ wp-content/themes/cvaikikaichildtheme/gettingStarted.php
@@ -6,32 +6,37 @@ get_header();
 
 <h1>GETTING STARTED</h1>
 <div class="gsBox">
-    <h2>what to bring</h2>
-    <h3>To join us for a lesson you'll only need a few things:</h3>
-    <p class="gsText">
-        - comfortable workout cloths<br>
-        - water bottle for hydration<br>
-        - three dollars to pay the front desk<br>
-        - a positive attitude and an open mind
-    </p>
+    <div class"middleBox">
+        <h2>what to bring</h2>
+        <h3>To join us for a lesson you'll only need a few things:</h3>
+        <p class="gsText">
+            - comfortable workout cloths<br>
+            - water bottle for hydration<br>
+            - three dollars to pay the front desk<br>
+            - a positive attitude and an open mind
+        </p>
+    </div>
 </div>
 <div class="gsBox">
-    <h2>dojo etiquette</h2>
-    <h3>To show respect while training here are a few rules to be aware of:</h3>
-    <p class="gsText">
-        take shoes and socks off before entering the dojo -<br>
-        bow upon entering and leaving the dojo -<br>
-        show respect to your training partner -<br>
-        don't forget to enjoy yourself -<br>
-    </p>
+    <div class"middleBox">
+        <h2>dojo etiquette</h2>
+        <h3>To show respect while training here are a few rules to be aware of:</h3>
+        <p class="gsText">
+            take shoes and socks off before entering the dojo -<br>
+            bow upon entering and leaving the dojo -<br>
+            show respect to your training partner -<br>
+            don't forget to enjoy yourself -<br>
+        </p>
+    </div>
 </div>
-<div class="gsbox">
-    <h2>where to find us</h2>
-    <h3>We practive at the comox rec center</h3>
-    <p class="gsText">
-        1855 Noel Avenue<br>
-        Comox, BC V9M2H4
-    </p>
+<div class="gsBox">
+    <div class"middleBox">
+        <h2>where to find us</h2>
+        <h3>We practive at the comox rec center</h3>
+        <p class="gsText">
+            1855 Noel Avenue<br>
+            Comox, BC V9M2H4
+        </p>
+    </div>
 </div>
-
 <?php get_footer(); ?>
